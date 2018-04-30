dc443a8a6d385581ed978a1226d453b5c712e015
ggravanis <ggravanis@csd.auth.gr>
2018-04-30 17:01:48 +0300

    Report update v2.0

--- src/machine_learning/plots.py
+++ src/machine_learning/plots.py
@@ -12,8 +12,8 @@ names = df['Method']
 names = np.array(names)
 print names
 
-df.plot.bar(width=0.7, figsize=(10, 10))
-plt.ylabel('accuracy %')
+df.plot.bar(width=0.7, figsize=(12, 12))
+plt.ylabel('AUC %')
 plt.title("Algorithm & balancing method benchmarcking diagram")
 plt.ylim((0.4, 1))
 plt.xticks(np.arange(len(names)), names, rotation=60)
