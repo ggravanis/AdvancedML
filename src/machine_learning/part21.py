402b19b76f345a2b20715761045abd5b916ff4d0
ggravanis <ggravanis@csd.auth.gr>
2018-05-27 09:20:36 +0300

    part21, bug fix. Script is ok now

--- src/machine_learning/part21.py
+++ src/machine_learning/part21.py
@@ -8,6 +8,7 @@ from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import SVC
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn import metrics
+from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 
 # Set the paths
@@ -18,23 +19,23 @@ save_path = "../../results/part21/"
 X_test = pd.read_csv(load_path + 'part1_test.csv', na_values=0, keep_default_na=True)
 X_test = pd.DataFrame(X_test).fillna(0)
 X_test = X_test.set_index(['Unnamed: 0'])
-X_test = X_test.iloc[:, 1:]
+X_test = X_test.iloc[:150, 1:]
 X_test = np.array(X_test)
 
 X_train = pd.read_csv(load_path + 'part1_train.csv', na_values=0, keep_default_na=True)
 X_train = pd.DataFrame(X_train).fillna(0)
 X_train = X_train.set_index(['Unnamed: 0'])
-X_train = X_train.iloc[:, 1:]
+X_train = X_train.iloc[:150, 1:]
 X_train = np.array(X_train)
 
 y_train = pd.read_csv(load_path + 'train_labels.csv', na_values=0, keep_default_na=True)
 y_train = pd.DataFrame(y_train).fillna(0)
-y_train = y_train.iloc[:, 1:]
+y_train = y_train.iloc[:150, 1:]
 # y_train = np.array(y_train)
 
 y_test = pd.read_csv(load_path + 'test_labels.csv', na_values=0, keep_default_na=True)
 y_test = pd.DataFrame(y_test).fillna(0)
-y_test = y_test.iloc[:, 1:]
+y_test = y_test.iloc[:150, 1:]
 
 
 def binary_to_text(df):
@@ -55,12 +56,14 @@ y_train = binary_to_text(y_train)
 
 if __name__ == "__main__":
 
-    clf = OneVsRestClassifier(estimator=SVC(kernel='linear', C=500, probability=True, verbose=False), n_jobs=-1)
+    clf = OneVsRestClassifier(estimator=SVC(kernel='linear', C=1, probability=True, verbose=True), n_jobs=-1)
+    # clf = OneVsRestClassifier(estimator=GaussianNB(), n_jobs=-1)
 
-    # X_train, X_test, y_train, y_test = train_test_split(X_test, y_test, random_state=7, test_size=0.3)
     mlb = MultiLabelBinarizer()
+    print
     y_train = mlb.fit_transform(y_train)
     y_test = mlb.transform(y_test)
+    print
     clf.fit(X_train, y_train)
     predictions = clf.predict(X_test)
 
